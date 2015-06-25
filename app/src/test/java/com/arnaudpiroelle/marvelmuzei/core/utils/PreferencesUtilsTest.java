19f90816cd30b92cd0e7d977cc9efd637b716368
Arnaud Piroelle <piroelle.arnaud@gmail.com>
2015-06-25 18:32:51 +0200

    Add CircleCI configurations

---
+++ app/src/test/java/com/arnaudpiroelle/marvelmuzei/core/utils/PreferencesUtilsTest.java
@@ -0,0 +1,12 @@
+package com.arnaudpiroelle.marvelmuzei.core.utils;
+
+import org.junit.Test;
+
+public class PreferencesUtilsTest {
+
+    @Test
+    public void should_return_false() throws Exception {
+
+
+    }
+}
